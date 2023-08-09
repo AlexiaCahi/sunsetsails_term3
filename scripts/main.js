5220b258009cde2545d3f750e4628b1fce3e58d5
AlexiaCahi <231245@virtualwindow.co.za>
2023-08-09 11:11:00 +0200

    Progress Milestone One

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+// ----------------------------------------------------------------
+// Trips array
+// ----------------------------------------------------------------
+
+const arrTrips = [
+    {
+      "name": "Madagascar",
+      "price": 10000,
+      "description": "Take a luxurious, tropical trip to Madagascar, this packagage is an all inclusive 9 day trip, children under the age of 12 eat free.",
+      "image": "madagascar.jpg"
+    },
+    {
+      "name": "Cyprus",
+      "price": 16000,
+      "description": "Take a one of a kind trip to Cyprus using Saunset Sails, at the low price of R16 000 you and your loved ones can enjoy a 10 day trip with an included buffet breakfast every morning.",
+      "image": "cyprus.jpeg"
+    },
+    {
+      "name": "Lebanon",
+      "price": 13000,
+      "description": "Enjoy an 8 day all inclusive cruise. At the price of R13 000 per person, you get to experience top quality service while on a luxurious trip to Lebanon.",
+      "image": "lebanon.jpg"
+    },
+    {
+      "name": "Rome",
+      "price": 23000,
+      "description": "A 12 day trip to Rome is at your fingertips. This trip is not all inculsive, however, breakfast and dinner are included. There are various different restuarants onboard where you can purchase your lunch. Take this oppertunity to experience Rome, Italy in all of its glory.",
+      "image": "rome.jpeg"
+    },
+    {
+      "name": "Mauritius",
+      "price": 19000,
+      "description": "Mauritius is an amazing island to explore, at the price of R19 000, you are able to enjoy a luxurious trip to this amazing island. Breakfast is included every morning as well as unlimited soft serve throughout the day. There are multiple options for lunch and dinner.",
+      "image": "mauritius.jpg"
+    },
+    {
+        "name": "Seychelles",
+        "price": 25000,
+        "description": "Explore the island of Seychelles, this package includes all meals, you however will need to purchase your own drinks. Lucky for you we have many different bars and stalls for you to do so.",
+        "image": "seychelles.jpeg"
+      }
+]
